63d6ca6e864aa54ed4f1bd7017be07d6637f3abb
MarceloGoncalves <marcelogoncalvescdd@gmail.com>
2018-08-21 15:07:31 -0300

    codigo incompleto de uma aplicao que cama uma segunda tela

---
+++ Varias-telas/app/src/main/java/com/example/aluno/variastelas/ActivityContatos.java
@@ -0,0 +1,13 @@
+package com.example.aluno.variastelas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityContatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contatos);
+    }
+}
---
+++ Varias-telas/app/src/main/java/com/example/aluno/variastelas/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.aluno.variastelas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void chamarTelaContatos(View view){
+
+        Intent intent = new Intent(this, ActivityContatos.class);
+        startActivity(intent);
+
+    }
+
+
+}
